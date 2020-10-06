d00f578c868ebace4cedf99bc8335f977fce4bf1
Lucas Poupaud <lp@2ms.tech>
2020-10-07 00:17:42 +0200

    Working setup HorusSlicer

--- standarditemwithslaves.h
+++ standarditemwithslaves.h
@@ -7,6 +7,7 @@ class StandardItemWithSlaves : public StandardItem
 public:
     StandardItemWithSlaves(const QString& name="", const QVariant &value=QVariant(), const QString& qmlFile="",  QList<QStandardItem*> slaves={}, int complexity=0, const QString& description="", const QString& unit="", const QVariant& min=QVariant(),const QVariant& max=QVariant(), const QVariantList& choiceList=QVariantList()):StandardItem(name, value, qmlFile, complexity,description,unit, min,max,choiceList){m_slaves.append(slaves);}
     inline void appendSlave(QStandardItem* slave){m_slaves.append(slave);updateSlave();}
+    inline void appendSlaves(QList<QStandardItem*> slave){m_slaves.append(slave);updateSlave();}
     virtual void updateSlave(int role=Qt::DisplayRole){Q_UNUSED(role);};
      virtual void setData(const QVariant &value, int role = Qt::UserRole + 1) override;
 protected:
